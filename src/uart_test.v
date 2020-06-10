1b84ae9fc93d6d868f1b6203f15b83037aec190c
itumeleng96 <37007962+itumeleng96@users.noreply.github.com>
2020-06-10 16:34:14 +0200

    Update uart_test.v

--- src/uart_test.v
+++ src/uart_test.v
@@ -1,23 +1,42 @@
 
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: IJ Malemela
+// 
+// Create Date: 10.06.2020 16:16:28
+// Design Name: SHAVADER
+// Module Name: top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: To test the UART Transmission and Reception
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
-module main(
-input wire clk,
-output wire TXD,
-input wire RXD,
-input wire [7:0] sw,
-output wire [7:0] LED
+module uart_module(
+input  CLK100MHZ,
+output  TXD,
+input  RXD,
+input  [7:0] SW,
+output [7:0] LED
 );
 
 
-
-
-wire [7:0] data_from_PC; wire [7:0] data_to_PC;
+wire [7:0] data_from_PC; 
+wire [7:0] data_to_PC;
 reg WR;
 parameter CLK_DIV = 24'h98967F;  //10 times per second
 reg [23:0] div = 1'b0;
 
   uart_tx_rx UART(
-      .clk(clk),
+      .clk(CLK100MHZ),
     .WR(WR),
     .DIN(data_to_PC),
     .DOUT(data_from_PC),
@@ -26,7 +45,7 @@ reg [23:0] div = 1'b0;
     .TXD(TXD),
     .RXD(RXD));
 
-  always @(posedge clk) begin
+  always @(posedge CLK100MHZ) begin
     if((div == CLK_DIV)) begin
       div <= {24{1'b0}};
       WR <= 1'b1;
@@ -38,6 +57,6 @@ reg [23:0] div = 1'b0;
   end
 
   assign LED = data_from_PC;
-  assign data_to_PC = sw;
+  assign data_to_PC = SW;
 
 endmodule
